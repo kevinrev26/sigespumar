d70ebe942c345205b42fec549b278cfba447f4a7
Kevin Rivera <kevinrev26@gmail.com>
2017-06-06 22:25:28 -0600

    Commit inicial

---
+++ src/main/java/com/sig23/sigespumar/SigespumarApplication.java
@@ -0,0 +1,12 @@
+package com.sig23.sigespumar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigespumarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigespumarApplication.class, args);
+	}
+}
