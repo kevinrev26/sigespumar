589ae787a598dce50c5adc6a312a4176c242feb0
Kevin Rivera <kevinrev26@gmail.com>
2017-06-26 01:12:01 -0600

    Finalizadas las vistas para los reportes

--- src/main/java/com/sig23/sigespumar/web/EstrategicoController.java
+++ src/main/java/com/sig23/sigespumar/web/EstrategicoController.java
@@ -1,6 +1,8 @@
 package com.sig23.sigespumar.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by kevin on 06-17-17.
@@ -11,5 +13,32 @@ import org.springframework.stereotype.Controller;
 * Descripción: 
 */
 @Controller
+@RequestMapping("/estrategico")
 public class EstrategicoController {
+
+    @RequestMapping(value = "/reporte",method = RequestMethod.GET)
+    public String reporte1(){
+        return "estrategico/r1";
+    }
+    @RequestMapping(value = "/reporte2",method = RequestMethod.GET)
+    public String reporte2(){
+        return "tactico/r2";
+    }
+    @RequestMapping(value = "/reporte3",method = RequestMethod.GET)
+    public String reporte3(){
+        return "estrategico/r3";
+    }
+    @RequestMapping(value = "/reporte4",method = RequestMethod.GET)
+    public String reporte4(){
+        return "estrategico/r4";
+    }
+    @RequestMapping(value = "/reporte5",method = RequestMethod.GET)
+    public String reporte5(){
+        return "estrategico/r5";
+    }
+    @RequestMapping(value = "/reporte6",method = RequestMethod.GET)
+    public String reporte6(){
+        return "estrategico/r6";
+    }
+
 }
--- src/main/java/com/sig23/sigespumar/web/TacticoController.java
+++ src/main/java/com/sig23/sigespumar/web/TacticoController.java
@@ -1,6 +1,8 @@
 package com.sig23.sigespumar.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by kevin on 06-17-17.
@@ -11,5 +13,31 @@ import org.springframework.stereotype.Controller;
 * Descripción: 
 */
 @Controller
+@RequestMapping("/tactico")
 public class TacticoController {
+
+    @RequestMapping(value = "/reporte",method = RequestMethod.GET)
+    public String reporte1(){
+        return "tactico/r1";
+    }
+    @RequestMapping(value = "/reporte2",method = RequestMethod.GET)
+    public String reporte2(){
+        return "tactico/r2";
+    }
+    @RequestMapping(value = "/reporte3",method = RequestMethod.GET)
+    public String reporte3(){
+        return "tactico/r3";
+    }
+    @RequestMapping(value = "/reporte4",method = RequestMethod.GET)
+    public String reporte4(){
+        return "tactico/r4";
+    }
+    @RequestMapping(value = "/reporte5",method = RequestMethod.GET)
+    public String reporte5(){
+        return "tactico/r5";
+    }
+    @RequestMapping(value = "/reporte6",method = RequestMethod.GET)
+    public String reporte6(){
+        return "tactico/r6";
+    }
 }
